382af464d6f66354a8d77ab71f7a35cdfe1c3a7b
Pavanpinisetti14 <nagapa1pinisetti@gmail.com>
2025-07-30 14:46:55 +0530

    update

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/api/history', async (req, res) => {
 });
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
